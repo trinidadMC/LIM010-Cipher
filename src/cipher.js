6d612402b1f81d67bd22e492143b5686dc8458e4
trinidadMC <triny.23.93@gmail.com>
2019-06-11 19:09:15 -0500

    avances en ccs y javascript

--- src/cipher.js
+++ src/cipher.js
@@ -1,18 +1,50 @@
-/* window.cipher = 
-  encode: (offset, string) => {}
+ /*window.cipher = 
+  encode:(offset, string) => {}
  let solved = "";
 
    for(let i=0;i<string.length;i++)
    const asiiNum = string[1].charCodeAt();
    if(asiiNum >= 65 && asciiNum <= 90){
      solved+= string.fromCharCode(asciiNum + offset)
-   }
- } return solved;
-},
+   };
+ return solved;
 
   decode: (offset, string) => {
-  },
-    /* Acá va tu código que descifra*/
-
+  };
+*/
+encode: (offset, string) => {
+  let solved = '';
+  const stringArr = string.split('');
+  stringArr.forEach((letra) => {
+    const asciiNum = letra.charCodeAt();
+    if (asciiNum >= 65 && asciiNum <= 90) {
+      const code = (asciiNum + 65 - parseInt(offset)) % 26 + 65;
+      solved += String.fromCharCode(code);
+    } else if (asciiNum >= 97 && asciiNum <= 122) {
+      const code = (asciiNum + 97 + parseInt(offset)) % 26 + 97;
+      solved += String.fromCharCode(code);
+    } else if (asciiNum === 32) {
+      solved += ' ';
+    }
+  });
+  return solved;
+}
+decode: (offset, string) => {
+  let solved = '';
+  const stringArr = string.split('');
+  stringArr.forEach((letra) => {
+    const asciiNum = letra.charCodeAt();
+    if (asciiNum >= 65 && asciiNum <= 90) {
+      const code = (asciiNum + 65 - parseInt(offset)) % 26 + 65;
+      solved += String.fromCharCode(code);
+    } else if (asciiNum >= 97 && asciiNum <= 122) {
+      const code = (asciiNum + 97 + parseInt(offset)) % 26 + 97;
+      solved += String.fromCharCode(code);
+    } else if (asciiNum === 32) {
+      solved += ' ';
+    }
+  });
+  return solved;
+}
  
  
--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ const primeracara = document.getElementById("primeracara");
 const password = document.getElementById("clave");
 const primerbutton = document.getElementById("btn1");
 const error = document.getElementById("error");
-/*DECLARACION DE MI SEGUNDACARA CON INTERACCION A LA PRIMERA */
-const segundacara = document.getElementById("segundacara")
+/*DECLARACION DE INTERACCION DE LA PRIMERA MAS ENLACE A LA SIGUIENTE*/
+const segundacara = document.getElementById("segundacara");
 let contador = 0;
 primerbutton.addEventListener("click", () => {
     if (password.value === "LABORATORIA") {
@@ -20,10 +20,35 @@ primerbutton.addEventListener("click", () => {
         error.innerHTML = "contraseña incorrecta";
     }
 });
-/*DECLARANDO MI TERCERA CARA Y BOTONES*/
+
+/*DECLARANDO MI SEGUNDA CARA Y CAJA DE TEXTO MAS BOTONES CIFRADO Y DESCIFRADO MAS ENLACE A LA SIGUIENTE CARA3*/
 const terceracara = document.getElementById("terceracara");
-segundobutton = document.getElementById("btn2");
-tercerbutton = document.getElementById("btn3");
-
-
+const output = document.getElementById("container");
+const textarea1 = document.getElementById("cajamensaje");
+const cifradobutton = document.getElementById("btn2");
+const descifradobutton = document.getElementById("btn3");
+const textarea2 = document.getElementById("cajaderesultado");
+cifradobutton.addEventListener("click", () => {
+    if (cajamensaje.value === " ") {
+        segundacara.classList.add("CARA2 hide");
+        terceracara.classList.remove("hide");
+        console.log("");
+    }
+    if (cajamensaje.value === " ") {
+        segundacara.classList.add("hide");
+        terceracara.classList.remove("hide");
+        console.log("");
+    }
+});
+/*DECLARANDO LA TERCERA CARA CON BOTONES DE SALIR Y VOLVER AL INICIO*/
+const terminarysalir = document.getElementById("btn4");
+const terminaryvolveralinicio = document.getElementById("btn5");
+terminarysalir.addEventListener("click", () => {
+    if (cajaresultado.value === " ");
+    terceracara.classList.add("hide CARA3");
+    console.log(" ");
+    if (cajaresultado.value===" " );
+    terceracara.classList.add("hide CARA3");
+    terceracara.classList.remove("CARA1");
+});
 
